8df450c4c43751ca520abd93745e9c5064f1a900
BwonK <ozenmain@gmail.com>
2025-04-26 17:37:21 +0300

    UniveristyDB

--- UniversityDB.sql
+++ UniversityDB.sql
@@ -1,11 +1,11 @@
---Veri Tabaný oluþturma kodlarý---
+ï»¿
 CREATE DATABASE universiteDB ;
 GO
 
 USE universiteDB ;
 GO
 
---TABLO OLUÞTURMA
+
 CREATE TABLE Kampus(
 
 	kampus_id INT PRIMARY KEY,
@@ -21,6 +21,7 @@ CREATE TABLE Bolum(
 	FOREIGN KEY (kampus_id) REFERENCES Kampus(kampus_id)
 
 );
+
 CREATE TABLE  Hoca(
 
 	hoca_id INT PRIMARY KEY,
@@ -37,21 +38,18 @@ CREATE TABLE Ogrenci(
 	ogrenci_adi VARCHAR(100),
 	ogrenci_soyadi VARCHAR(100),
 	bolum_id INT,
-	danýsman_id INT,
+	danisman_id INT,
 	FOREIGN KEY (bolum_id) REFERENCES Bolum(bolum_id),
-	FOREIGN KEY (danýsman_id) REFERENCES Hoca(hoca_id)
+	FOREIGN KEY (danisman_id) REFERENCES Hoca(hoca_id)
   
  );
 	
-CREATE TABLE derslik(
+CREATE TABLE Derslik(
 
-derslik_id INT PRIMARY KEY ,
-derselik_adi VARCHAR (100),
-kapasite INT
-);
-
-
-
+	derslik_id INT PRIMARY KEY ,
+	derselik_adi VARCHAR (100),
+	kapasite INT
+	);
 
 CREATE TABLE Ders(
 	
@@ -65,7 +63,6 @@ CREATE TABLE Ders(
 	FOREIGN KEY (kampus_id) REFERENCES Kampus(kampus_id)
 );
 
-
 CREATE TABLE Ogrenci_Ders(
 	ogrenci_id INT,
 	ders_id INT,
@@ -75,64 +72,63 @@ CREATE TABLE Ogrenci_Ders(
 
 );
 
+CREATE TABLE Sinav (
 
-CREATE TABLE Sýnav (
-
-	sýnav_id INT PRIMARY KEY ,
+	sinav_id INT PRIMARY KEY ,
 	ders_id INT ,
-	sýnav_tarihi DATE ,
+	sinav_tarihi DATE ,
 	FOREIGN KEY (ders_id) REFERENCES Ders(ders_id)
 
 );
 
 CREATE TABLE Notlar (
-   ogrenci_id INT,
-   sýnav_id INT,
-   notu INT ,
-   PRIMARY KEY (ogrenci_id , sýnav_id),
-   FOREIGN KEY (ogrenci_id) REFERENCES Ogrenci(ogrenci_id),
-   FOREIGN KEY(sýnav_id) REFERENCES Sýnav(sýnav_id)
+    ogrenci_id INT,
+    sinav_id INT,
+    notu INT ,
+    PRIMARY KEY (ogrenci_id , sinav_id),
+    FOREIGN KEY (ogrenci_id) REFERENCES Ogrenci(ogrenci_id),
+    FOREIGN KEY(sinav_id) REFERENCES Sinav(sinav_id)
 
 );
 
 
 CREATE TABLE Devamsizlik(
-   ogrenci_id INT, 
-   ders_id INT ,
-   devamsizlik_sayýsý INT ,
-   PRIMARY KEY (ogrenci_id , ders_id),
-   FOREIGN KEY (ogrenci_id) REFERENCES Ogrenci(ogrenci_id),
-   FOREIGN KEY(ders_id) REFERENCES Ders(ders_id)
+    ogrenci_id INT, 
+    ders_id INT ,
+    devamsizlik_sayisi INT ,
+    PRIMARY KEY (ogrenci_id , ders_id),
+    FOREIGN KEY (ogrenci_id) REFERENCES Ogrenci(ogrenci_id),
+    FOREIGN KEY(ders_id) REFERENCES Ders(ders_id)
 
 );
 
 INSERT INTO Kampus VALUES 
 	
-	(1 , 'Merkez Kampüs'),
-	(2 , 'Teknoloji Kampüsü')
+	(1 , 'Merkez KampÃ¼s'),
+	(2 , 'Teknoloji KampÃ¼sÃ¼')
 	
 INSERT INTO  Bolum VALUES
 
-	(1 , 'PC Mühendisliði' , 1),
-	(2 , 'Elektirik Mühendisliði' , 1),
-	(3 , 'Makina Mühendisliði' ,2 );
+	(1 , 'PC MÃ¼hendisliÃ°i' , 1),
+	(2 , 'Elektirik MÃ¼hendisliÃ°i' , 1),
+	(3 , 'Makina MÃ¼hendisliÃ°i' ,2 );
 
 INSERT INTO Hoca VALUES
 
-	(101 , 'Yiðit' , 'Özen',1),
-	(102 , 'Merve' , 'Özkan' ,1),
-	(103 , 'Koray' , 'Taþtan' ,2),
+	(101 , 'YiÄŸit' , 'Ã–zen',1),
+	(102 , 'Merve' , 'Ã–zkan' ,1),
+	(103 , 'Koray' , 'TaÃ¾tan' ,2),
 	(104 , 'Mehmet' , 'Naci' ,2),
 	(105 , 'Kamil' , 'Ersoy' ,3);
 
 INSERT INTO Ogrenci VALUES
 	(201 , 'Necat' , 'Ham' , 1 , 101),
-	(202 , 'Hanife' , 'Koç' , 1 , 102),
+	(202 , 'Hanife' , 'KoÃ§' , 1 , 102),
 	(203 , 'Fetih' , 'Ulu' , 2 , 103),
 	(204 , 'Ahmet' , 'Deniz' , 2 , 104),	
 	(205 , 'Fatma' , 'Er' , 3 , 105);
 
-INSERT INTO derslik VALUES
+INSERT INTO Derslik VALUES
 	(1, 'A101' , 40),
 	(2, 'B202' , 30),
 	(3, 'C303' , 35),
@@ -140,7 +136,7 @@ INSERT INTO derslik VALUES
 	(5, 'E505' , 20);
 
 INSERT INTO Ders VALUES
-	(301 ,'Veri Tabaný', 101 , 1, 1),
+	(301 ,'Veri TabanÄ±', 101 , 1, 1),
 	(302 ,'Algoritma', 102 , 2, 1),
 	(303 ,'Devre Teorsi', 103 , 3, 2),
 	(304 ,'Elektronik', 104 , 4, 2),
@@ -152,7 +148,7 @@ INSERT INTO Ogrenci_Ders VALUES
 	(202,301),(203,303),
 	(204,304),(205,305)
 
-INSERT INTO Sýnav VALUES
+INSERT INTO Sinav VALUES
 	
 	(401, 301 , '2025-04-01'),
 	(402, 301 , '2025-04-15'),
